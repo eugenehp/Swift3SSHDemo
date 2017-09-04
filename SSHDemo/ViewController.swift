60ac07dbd52c44397f4b773188cffc5bb96f91ff
Eugene Hauptmann <eugene.hp2012@gmail.com>
2017-09-03 20:59:37 -0700

    Update ViewController.swift

--- SSHDemo/ViewController.swift
+++ SSHDemo/ViewController.swift
@@ -9,23 +9,31 @@ class ViewController: UIViewController, NMSSHChannelDelegate {
         super.viewDidLoad()
         
         
-        let host = "10.10.10.10:22"
-        let username = "root"
-        let password = "password"
+        let host = "10.0.1.15:22"
+        let username = "admin"
+        let password = "123"
         let session = NMSSHSession(host: host, andUsername: username)
         print("Trying to connect now..")
         session?.connect()
         if session?.isConnected == true
         {
             print("Session connected")
+            session?.channel.delegate = self
+            session?.channel.ptyTerminalType = .vanilla
+            session?.channel.requestPty = false
             session?.authenticate(byPassword:password)
             
             do{
-                try session?.channel.execute("echo $PATH")
+                try session?.channel.startShell()
+                let a = try session?.channel.write("echo $PATH\n")
+                print(a)
                 print(session?.channel.lastResponse ?? "no respone of last command")
             }catch{
                 print("Error ocurred!!")
             }
+            
+            //For other types
+            //session.authenticateByPassword(password)
         }
         //session?.disconnect()
     }
